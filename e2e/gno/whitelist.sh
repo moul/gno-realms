7bd2d6600fded0a6880f94bd379153eb2172ca01
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2026-04-17 15:33:17 +0200

    fix(e2e): use MsgCall for AddRelayer in whitelist.sh

--- e2e/gno/whitelist.sh
+++ e2e/gno/whitelist.sh
@@ -12,25 +12,17 @@ echo "Relayer address: $RELAYER_ADDR"
 TEST1_SEED="source bonus chronic canvas draft south burst lottery vacant surface solve popular case indicate oppose farm nothing bullet exhibit title speed wink action roast"
 printf "%s\n\n" "$TEST1_SEED" | gnokey add test1 --recover --insecure-password-stdin --force 2>&1
 
-cat > /tmp/whitelist-relayer.gno << EOF
-package main
-
-import "gno.land/r/aib/ibc/core"
-
-func main() {
-    core.AddRelayer(cross, "$RELAYER_ADDR")
-}
-EOF
-
 echo "Whitelisting relayer $RELAYER_ADDR on gno:26657..."
-printf "\n" | gnokey maketx run \
+printf "\n" | gnokey maketx call \
     -gas-fee 1000000ugnot \
     -gas-wanted 90000000 \
     -broadcast \
     -chainid dev \
     -remote gno:26657 \
     -insecure-password-stdin \
-    test1 \
-    /tmp/whitelist-relayer.gno
+    -pkgpath gno.land/r/aib/ibc/core \
+    -func AddRelayer \
+    -args "$RELAYER_ADDR" \
+    test1
 
 echo "Relayer $RELAYER_ADDR whitelisted"
